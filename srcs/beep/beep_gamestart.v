cfc51c4922b145c0339e8d6e1710a7d4e5a0785f
KWarRiN <13566361306@163.com>
2025-06-21 00:22:44 +0800

    beep_gamestart.v

--- srcs/beep/beep_gamestart.v
+++ srcs/beep/beep_gamestart.v
@@ -24,15 +24,12 @@ module beep_gamestart(
 
     parameter TIME_125ms = 24'd12499999, // 125 ms
                 A4 = 19'd227273, // 440 Hz
-                D5 = 19'd170357, // 587
-                C5 = 19'd191204, // 523
-                B4 = 19'd202428, // 494
-                FS_4 = 19'd270269, // 370
-                G4 = 19'd255101, // 392
-                D4 = 19'd378787, // 264
-                E4 = 19'd303030, // 330
-                F4 = 19'd286532, // 349
-                C4 = 19'd381678; // 262
+                A14=19'd214519, // 466.16Hz
+                G4 = 19'd255101, // 392Hz
+                D4 = 19'd378787, // 264Hz
+                E4 = 19'd303030, // 330Hz
+                F4 = 19'd286532, // 349Hz
+                C4 = 19'd381678; // 262Hz
 
     assign duty_data = freq_data >> 1'b1;
 
@@ -91,7 +88,65 @@ always @(posedge clk or posedge rst) begin
         freq_data <= 19'd0;
     end else begin
         case (cnt_125ms) // 根据不同的时间段，为该变量赋值不同的频率值
-            default: freq_data <= D4;
+            7'd0: freq_data <= D4;  
+            7'd1: freq_data <= E4;
+            7'd2: freq_data <= F4;
+            7'd3: freq_data <= G4;
+            7'd4: freq_data <= A4;  
+            7'd5: freq_data <= A4;
+            7'd6: freq_data <= A4;
+            7'd7: freq_data <= A4;
+            7'd8: freq_data <= D4; 
+            7'd9: freq_data <= D4;
+            7'd10: freq_data <= D4; 
+            7'd11: freq_data <= D4;
+            7'd12: freq_data <= G4; 
+            7'd13: freq_data <= G4;
+            7'd14: freq_data <= G4;  
+            7'd15: freq_data <= G4;
+            7'd16: freq_data <= C4; 
+            7'd17: freq_data <= C4;
+            7'd18: freq_data <= F4;  
+            7'd19: freq_data <= F4;
+            7'd20: freq_data <= F4;
+            7'd21: freq_data <= F4;
+            7'd22: freq_data <= D4;  
+            7'd23: freq_data <= D4;
+            7'd24: freq_data <= E4;
+            7'd25: freq_data <= E4;
+            7'd26: freq_data <= F4;  
+            7'd27: freq_data <= F4;
+            7'd28: freq_data <= F4;
+            7'd29: freq_data <= F4;
+            7'd30: freq_data <= G4;  
+            7'd31: freq_data <= G4;
+            7'd32: freq_data <= A4;
+            7'd33: freq_data <= A4;
+            7'd34: freq_data <= F4;  
+            7'd35: freq_data <= F4;
+            7'd36: freq_data <= A14;  
+            7'd37: freq_data <= A14;
+            7'd38: freq_data <= A14;
+            7'd39: freq_data <= A14;
+            7'd40: freq_data <= E4;  
+            7'd41: freq_data <= E4;
+            7'd42: freq_data <= E4;
+            7'd43: freq_data <= E4;
+            7'd44: freq_data <= A4;  
+            7'd45: freq_data <= A4;
+            7'd46: freq_data <= A4;
+            7'd47: freq_data <= A4;
+            7'd48: freq_data <= D4;
+            7'd49: freq_data <= D4;
+            7'd50: freq_data <= G4;
+            7'd51: freq_data <= G4;            
+            7'd52: freq_data <= G4;
+            7'd53: freq_data <= G4;
+            7'd54: freq_data <= G4;
+            7'd55: freq_data <= G4;            
+            7'd56: freq_data <= G4;
+            7'd57: freq_data <= G4;
+            default: freq_data <= 19'd0;
         endcase
     end
 end
