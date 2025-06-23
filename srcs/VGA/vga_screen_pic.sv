24906ba70cb9e798200bdc8e76f7c92d89dbf64b
youngthen <3240105586@zju.edu.cn>
2025-06-23 17:44:29 +0800

    Update screen_pix, Create background.coe
    添加了背景的滚动

--- srcs/VGA/vga_screen_pic.sv
+++ srcs/VGA/vga_screen_pic.sv
@@ -4,6 +4,7 @@ module vga_screen_pic(
     input wire clk,
     input wire [1:0] gamemode,
     input wire [8:0] player_y,
+    input wire clk_60hz,
     input logic [9:0] [9:0] obstacle_x_game_left,
     input logic [9:0] [9:0] obstacle_x_game_right,
     input logic [9:0] [8:0] obstacle_y_game_up,
@@ -30,12 +31,16 @@ module vga_screen_pic(
               SCREEN_W_PIC = 10'd640, // VGA 宽度 (VGA width)
               PLAYER_PIC = 10'd40; // Player image size
 
+    localparam SCROLL_SPEED = 4;
+
+    reg [9:0] position_changed = 0; // 用于滚动背景的变量 (Variable for scrolling background)
     // Wire declarations for ROM data (ROM数据线声明)
-    wire [11:0] game_start_data, player_out_data, game_over_data;
+    wire [11:0] game_start_data, player_out_data, game_over_data, background_data;
     // ROM address declarations (ROM地址声明)
     wire [18:0] pic_romaddrStart; // 大图片gamestart的 ROM 地址 (Start screen ROM address)
     reg [15:0] pic_romaddrOver; // 小图片gameover的ROM地址 (Game Over ROM address)
     reg [10:0] pic_romaddrPlayer; // Player的ROM地址 (Player ROM address)
+    reg [18:0] pic_romaddrBackground;    
 
     // Instance of ROM blocks (ROM模块实例化)
     player player_rom (
@@ -53,13 +58,15 @@ module vga_screen_pic(
       .addra(pic_romaddrOver),
       .douta(game_over_data)
     );
-
+    background background_rom (
+        .clka(clk),    // input wire clka
+        .addra(pic_romaddrBackground),  // input wire [18 : 0] addra
+        .douta(background_data)  // output wire [11 : 0] douta
+    );
     // (直接赋值 pic_romaddrStart，因为它直接来源于 pix_x, pix_y 和 SCREEN_W_PIC)
     assign pic_romaddrStart = pix_x + pix_y * SCREEN_W_PIC;
-
-
-    // (计算玩家和游戏结束图片的ROM地址。无论它们是否显示，这些地址都应被计算，
-    // 因为 blk_mem_gen 模块会持续从这些地址读取数据。)
+    assign pic_romaddrBackground = (pix_y >= UPPER_BOUND && pix_y < LOWER_BOUND) ?  (pix_x + position_changed) % SCREEN_W_PIC + (pix_y - UPPER_BOUND) * SCREEN_W_PIC : 0; // In-game background ROM address
+    // 计算玩家和游戏结束图片的ROM地址
     always_comb begin
         pic_romaddrPlayer = (pix_x >= PLAYER_X && pix_x < PLAYER_X + PLAYER_SIZE &&
                              pix_y >= player_y && pix_y < player_y + PLAYER_SIZE) ?
@@ -67,6 +74,16 @@ module vga_screen_pic(
         pic_romaddrOver = (pix_x >= GAMEOVER_X && pix_x < GAMEOVER_X + H_PIC &&
                            pix_y >= GAMEOVER_Y && pix_y < GAMEOVER_Y + H_PIC) ?
                           (pix_x - GAMEOVER_X) + (pix_y - GAMEOVER_Y) * H_PIC : 0; // Default to 0 if out of bounds
+    end
+
+
+    always @(posedge clk_60hz)begin
+            if(gamemode == 2'b00)begin
+                position_changed <= 0;
+            end
+            else if(gamemode == 2'b01)begin
+                position_changed <= (position_changed + SCROLL_SPEED) % SCREEN_W_PIC ; // Scroll left (向左滚动)
+            end
     end
 
     // State signal for pixel type (像素类型状态信号)
@@ -83,14 +100,11 @@ module vga_screen_pic(
     //7: Paused screen (暂停画面)
     always_comb begin
         pixel_state = 3'd0; // Default to background (默认为背景)
-
-        //(游戏结束画面)
         if (gamemode == 2'b00) begin
             pixel_state = 3'd6; //(初始画面)
         end
         // In-game screen (游戏进行画面) - gamemode 2'b01
         else if (gamemode == 2'b01) begin
-            //游戏内边界优先级最高
             if (pix_y <= UPPER_BOUND || pix_y >= LOWER_BOUND) begin
                 pixel_state = 3'd0; // Border (边界)
             end
@@ -109,7 +123,7 @@ module vga_screen_pic(
                     end
                 end
                 if (pixel_state == 3'd0) begin
-                    pixel_state = 3'd5;//TODO游戏内背景
+                    pixel_state = 3'd5;//游戏内背景
                 end
             end
         end
@@ -158,7 +172,7 @@ module vga_screen_pic(
                     3'd0: rgb = DEFAULT_COLOR; // Border (边界) or Default (默认)
                     3'd1: rgb = COLOR_OBSTACLE; // Obstacle (障碍物)
                     3'd2: rgb = player_out_data; // Player (玩家)
-                    3'd5: rgb = COLOR_INGAME; // In-game background (游戏内背景)
+                    3'd5: rgb = background_data; // In-game background (游戏内背景)
                     default: rgb = DEFAULT_COLOR; // Fallback (回退)
                 endcase
             end
